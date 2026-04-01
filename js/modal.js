80f2afac846dc737d0a788ac0b6d933bcb564850
limkitea00 <limkitea00@gmail.com>
2026-04-01 10:45:11 +0900

    stxclone js study

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,10 @@
 window.addEventListener("load", function () {
+  // 모달창 닫기
+  const modalWrap = this.document.querySelector(".modal-wrap")
+  const modalClose = this.document.querySelector(".modalClose")
+  modalClose.addEventListener("click" , function(){
+    modalWrap.style.display = "none";
+  })
   // gotop 스크롤 이벤트
   // 변수 선언
   const goTop = this.document.querySelector("#gotop");
